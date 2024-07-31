8134f60789d6d9ffa95fb7195336c3c57f2c24b7
Aditya Bhate <85818935+Aditya-Bhate@users.noreply.github.com>
2024-07-31 16:28:41 -0400

    Update scripts.py

--- notifications/scripts.py
+++ notifications/scripts.py
@@ -4,6 +4,12 @@ from django.template import Context
 from django.template.loader import get_template
 from django.conf import settings
 
+def send_account_verification_mail(subject, first_name, verification_link, to_emails, from_email=settings.EMAIL_HOST_USER):
+    mail_content = {'first_name': first_name, "verification_link":verification_link}
+    txt_template_path = "notifications/templates/verify_email.txt"
+    html_template_path = "notifications/templates/verify_otp.html"
+    print(verification_link)
+    send_mail_with_template(mail_content, txt_template_path, html_template_path, subject, from_email, to_emails)
 
 def send_mail_with_template(mail_content, txt_template_path, html_template_path, subject, from_email, to_emails):
     with open(settings.ROOT_DIR + txt_template_path) as f:
@@ -12,12 +18,3 @@ def send_mail_with_template(mail_content, txt_template_path, html_template_path,
     html_template = get_template(settings.ROOT_DIR +html_template_path).render(mail_content)
     message.attach_alternative(html_template, 'text/html')
     message.send()
-
-
-
-def send_account_verification_mail(subject, first_name, verification_link, to_emails, from_email=settings.EMAIL_HOST_USER):
-    mail_content = {'first_name': first_name, "verification_link":verification_link}
-    txt_template_path = "notifications/templates/verify_email.txt"
-    html_template_path = "notifications/templates/verify_otp.html"
-    print(verification_link)
-    send_mail_with_template(mail_content, txt_template_path, html_template_path, subject, from_email, to_emails)
