bfe1a3a995d16cab211bbb12127c16d2d4cf4f1a
Aditya Bhate <85818935+Aditya-Bhate@users.noreply.github.com>
2024-07-31 12:26:11 -0400

    Update urls.py

--- info_pages/urls.py
+++ info_pages/urls.py
@@ -5,7 +5,7 @@ app_name = 'info_pages'
 
 urlpatterns = [
     path('about-us', AboutUsView.as_view(), name='about_us'),   #abouts us view
-    path('contact-us', ContactUsView.as_view(), name='contact_us'),
     path('privacy-policy', PrivacyPolicyView.as_view(), name='privacy_policy'),
+    path('contact-us', ContactUsView.as_view(), name='contact_us'),
     path('terms-and-conditions', TermsAndConditionsView.as_view(), name='terms_n_conditions'),
 ]
